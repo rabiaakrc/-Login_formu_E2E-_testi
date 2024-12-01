fb26f8678300484757070e960cd216e88c3a95b9
rabiaakrc <rabiakaraca33@gmail.com>
2024-12-01 16:24:36 +0300

    file3

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -6,36 +6,36 @@ describe("Login form - validation errors", () => {
   it("should show error message for invalid email", () => {
     cy.get('input[type="email"]').type("invalidemail"); 
     cy.get('input[type="password"]').type("Password1"); 
-    cy.get("button").should("be.disabled"); // Buton disabled olmalı
+    cy.get("button").should("be.disabled"); 
     cy.get(".email-error")
-      .should("have.css", "display", "block") // Hata mesajı görünür olmalı
-      .and("contain", "Geçerli bir email giriniz."); // Hata mesajı kontrolü
+      .should("have.css", "display", "block") 
+      .and("contain", "Geçerli bir email giriniz."); 
   });
   
   it("should show error message for invalid password", () => {
-    cy.get('input[type="email"]').type("test@example.com"); // Geçerli email giriliyor
-    cy.get('input[type="password"]').type("123"); // Geçersiz şifre
-    cy.get("button").should("be.disabled"); // Buton disabled olmalı
+    cy.get('input[type="email"]').type("test@example.com"); 
+    cy.get('input[type="password"]').type("123"); 
+    cy.get("button").should("be.disabled"); 
     cy.get(".password-error")
-      .should("be.visible") // Hata mesajının görünür olduğundan emin ol
+      .should("be.visible") 
       .and(
         "contain",
         "Şifre en az 8 karakter, bir büyük harf ve bir sayı içermelidir."
-      ); // Hata mesajı kontrolü
+      ); 
   });
 
   it("should show error message for not accepting terms", () => {
-    cy.get('input[type="email"]').type("test@example.com"); // Geçerli email
-    cy.get('input[type="password"]').type("Password1"); // Geçerli şifre
-    cy.get('input[type="checkbox"]').uncheck(); // Şartları kabul etme
-    cy.get("button").should("be.disabled"); // Buton disabled olmalı
+    cy.get('input[type="email"]').type("test@example.com"); 
+    cy.get('input[type="password"]').type("Password1"); 
+    cy.get('input[type="checkbox"]').uncheck(); 
+    cy.get("button").should("be.disabled"); 
     cy.get(".terms-error")
-      .should("be.visible") // Hata mesajının görünür olduğundan emin ol
-      .and("contain", "Şartları kabul etmelisiniz."); // Hata mesajı kontrolü
+      .should("be.visible") 
+      .and("contain", "Şartları kabul etmelisiniz."); 
   });
 
   it("should submit form and navigate to success page", () => {
-    cy.get('input[type="email"]').type("test@example.com"); // Geçerli email
+    cy.get('input[type="email"]').type("test@example.com"); 
     cy.get('input[type="password"]').type("Password1"); 
     cy.get('input[type="checkbox"]').check(); 
     cy.get("button").should("not.be.disabled");
